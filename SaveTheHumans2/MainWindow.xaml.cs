1b6adf17329f84abfdeac0d9f0b1f5e0aa413414
Jatudrei <yehoshua.kahan@gmail.com>
2018-08-02 22:00:18 -0400

    Corrected name of enemy template resource

--- SaveTheHumans2/MainWindow.xaml.cs
+++ SaveTheHumans2/MainWindow.xaml.cs
@@ -34,8 +34,10 @@ namespace SaveTheHumans2
 
         private void AddEnemy()
         {
-            ContentControl enemy = new ContentControl();
-            enemy.Template = Resources["EnemyTemplate"] as ControlTemplate;
+            ContentControl enemy = new ContentControl
+            {
+                Template = Resources["enemyTemplate"] as ControlTemplate
+            };
             AnimateEnemy(enemy, 0, playArea.ActualWidth - 100, "(Canvas.Left)");
             AnimateEnemy(enemy, random.Next((int)playArea.ActualHeight - 100), random.Next((int)playArea.ActualHeight - 100), "(Canvas.Top)");
             playArea.Children.Add(enemy);
