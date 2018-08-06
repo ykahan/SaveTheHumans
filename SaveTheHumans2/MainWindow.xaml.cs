423dacadb66610081f268ac4234df69bcb06763e
Jatudrei <yehoshua.kahan@gmail.com>
2018-08-06 13:09:07 -0400

    Added code to enable control of the human, and to end the game if the timer runs out or the human runs into an alien

--- SaveTheHumans2/MainWindow.xaml.cs
+++ SaveTheHumans2/MainWindow.xaml.cs
@@ -92,6 +92,13 @@ namespace SaveTheHumans2
             AnimateEnemy(enemy, 0, playArea.ActualWidth - 100, "(Canvas.Left)");
             AnimateEnemy(enemy, random.Next((int)playArea.ActualHeight - 100), random.Next((int)playArea.ActualHeight - 100), "(Canvas.Top)");
             playArea.Children.Add(enemy);
+            enemy.MouseEnter += Enemy_MouseEnter;
+        }
+
+        private void Enemy_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (humanCaptured)
+                EndTheGame();
         }
 
         private void AnimateEnemy(ContentControl enemy, double from, double to, string propertyToAnimate)
@@ -107,6 +114,51 @@ namespace SaveTheHumans2
             Storyboard.SetTargetProperty(animation, new PropertyPath(propertyToAnimate));
             storyboard.Children.Add(animation);
             storyboard.Begin();
+        }
+
+        private void human_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (enemyTimer.IsEnabled)
+            {
+                humanCaptured = true;
+                human.IsHitTestVisible = false;
+            }
+        }
+
+        private void target_MouseEnter(object sender, MouseEventArgs e)
+        {
+            progressBar.Value = 0;
+            Canvas.SetLeft(target, random.Next(100, (int)playArea.ActualWidth - 100));
+            Canvas.SetTop(target, random.Next(100, (int)playArea.ActualWidth - 100));
+            Canvas.SetLeft(human, random.Next(100, (int)playArea.ActualWidth - 100));
+            Canvas.SetTop(human, random.Next(100, (int)playArea.ActualWidth - 100));
+            humanCaptured = false;
+            human.IsHitTestVisible = true;
+        }
+
+        private void playArea_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point pointerPosition = e.GetPosition(null);
+            Point relativePosition = grid.TransformToVisual(playArea).Transform(pointerPosition);
+            if ((Math.Abs(relativePosition.X - Canvas.GetLeft(human)) > human.ActualWidth * 3) ||
+                (Math.Abs(relativePosition.Y - Canvas.GetTop(human)) > human.ActualHeight * 3))
+            {
+                humanCaptured = false;
+                human.IsHitTestVisible = true;
+            }
+            else
+            {
+                Canvas.SetLeft(human, relativePosition.X - human.ActualWidth / 2);
+                Canvas.SetTop(human, relativePosition.Y - human.ActualHeight / 2);
+            }
+        }
+
+        private void playArea_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (humanCaptured)
+            {
+                EndTheGame();
+            }
         }
     }
 }
